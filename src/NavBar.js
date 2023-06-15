f3ff15e17c4054294b399733449f063c8a68dcce
dima <87605146+konata12@users.noreply.github.com>
2023-06-15 22:00:16 +0300

    ok

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Jopa BLog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
